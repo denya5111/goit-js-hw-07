a8279683abbe7df3dc0f886498a7a01b0ca32547
Denys Denysiuk <denya5111@gmail.com>
2023-03-06 00:24:48 +0100

    homework v 1.0

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function createListItemsMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" onclick="return false"/>
+</a>;
+</div>`
+    )
+    .join('');
+}
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = createListItemsMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
