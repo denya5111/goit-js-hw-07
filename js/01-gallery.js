f7bd1c01b5690e9e9fb9b75c67cb6e2f322ddaae
Denys Denysiuk <denya5111@gmail.com>
2023-03-05 23:36:01 +0100

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function createListItemsMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" onclick="return false">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join('');
+}
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = createListItemsMarkup(galleryItems);
+
+galleryContainer.addEventListener('click', handlerClickImg);
+
+function handlerClickImg(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const modal = basicLightbox.create(`<img width="1280" src="${event.target.dataset.source}">`);
+
+  if (modal.show()) {
+    document.addEventListener('keydown', event => {
+      if (event.code === 'Escape') {
+        modal.close();
+      }
+    });
+  }
+}
